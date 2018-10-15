766f45c146b9787ef619e4dfa60c53041413ce1d
ialford <ialford@nd.edu>
2018-10-15 11:26:36 -0400

    commenting out the Airbrake initializer as app was never tracking errors

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,8 +1,5 @@
-Airbrake.configure do |config|
-  # config.project_id = true
-  config.api_key = Rails.application.secrets.airbrake_key
-  config.host    = "errbit.library.nd.edu"
-  config.port    = 443
-  config.secure  = config.port == 443
-  config.user_attributes = [:id, :username, :name]
-end
+# Airbrake.configure do |config|
+#   config.project_key = <%= ENV["SIPHON_ERRBIT_KEY"] %>
+#   config.project_id = true
+#   config.host    = "https://errbit.library.nd.edu:443"
+# end
