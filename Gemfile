e8c35c3ed751e4e4f52b2442a827144e4b306002
wsill <wsill@nd.edu>
2019-06-13 10:39:57 -0400

    Esu 930 (#6)
    * Full Deploy

--- Gemfile
+++ Gemfile
@@ -1,26 +1,21 @@
 source 'https://rubygems.org'
 
+gem 'bigdecimal'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', "~> 4.2.0"
 
-
 #gem 'hesburgh_assets', :git => 'git@git.library.nd.edu:assets'
 gem "hesburgh_infrastructure", github: "ndlib/hesburgh_infrastructure"
-
 gem "therubyracer"
-
-
 gem 'american_date'
 
 # db backends
-gem 'mysql2', "~> 0.3.11"
-
+gem 'mysql2'
 
 # authentication
 gem 'rubycas-client'
 gem 'devise'
 gem 'devise_cas_authenticatable'
-
 
 # LDAP lookups
 gem 'net-ldap'
--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,8 +1,8 @@
-Airbrake.configure do |config|
+#Airbrake.configure do |config|
   # config.project_id = true
-  config.api_key = Rails.application.secrets.airbrake_key
-  config.host    = "errbit.library.nd.edu"
-  config.port    = 443
-  config.secure  = config.port == 443
-  config.user_attributes = [:id, :username, :name]
-end
+#  config.api_key = Rails.application.secrets.airbrake_key
+#  config.host    = "errbit.library.nd.edu"
+#  config.port    = 443
+#  config.secure  = config.port == 443
+#  config.user_attributes = [:id, :username, :name]
+#end
