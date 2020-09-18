d709dbeb645629c871642172839fc6907bb0b74b
Bill Sill <wsill@nd.edu>
2020-09-18 10:09:14 -0400

    Another test

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 sed --version
 
 echo "Config files for service"
-sed -i 's~{{ database_host }}~""$DB_HOST"~g' "$APP_DIR/config/database.yml"
+sed -i 's/{{ database_host }}/'"$DB_HOST"'/g' "$APP_DIR/config/database.yml"
 sed -i 's/{{ database_username }}/'"$DB_NAME"'/g' "$APP_DIR/config/database.yml"
 sed -i 's/{{ database_password }}/'"$DB_PASSWORD"'/g' "$APP_DIR/config/database.yml"
 sed -i 's/{{ auth_server_id }}/'"$AUTH_SERVER_ID"'/g' "$APP_DIR/config/secrets.yml"
@@ -14,4 +14,5 @@ sed -i 's/{{ redirect_url }}/'"$REDIRECT_URL"'/g' "$APP_DIR/config/secrets.yml"
 sed -i 's/{{ secret_key_base }}/'"$SECRET_KEY_BASE"'/g' "$APP_DIR/config/secrets.yml"
 
 echo "Start Service"
+# source /etc/nginx/envvars
 exec nginx
