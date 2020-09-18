7bb78b931e3b79f6f9057df0cd1ed7c438b3ebb6
Bill Sill <wsill@nd.edu>
2020-09-18 08:43:23 -0400

    try to fix sed error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 set -e
 
-echo "Modify database file"
+echo "Config files for service"
 sed -i 's/{{ database_host }}/'"$DB_HOST"'/g' "$APP_DIR/config/database.yml"
 sed -i 's/{{ database_username }}/'"$DB_NAME"'/g' "$APP_DIR/config/database.yml"
 sed -i 's/{{ database_password }}/'"$DB_PASSWORD"'/g' "$APP_DIR/config/database.yml"
-
-echo "Modify secrets file"
 sed -i 's/{{ auth_server_id }}/'"$AUTH_SERVER_ID"'/g' "$APP_DIR/config/secrets.yml"
 sed -i 's/{{ base_auth_url }}/'"$BASE_AUTH_URL"'/g' "$APP_DIR/config/secrets.yml"
 sed -i 's/{{ client_id }}/'"$CLIENT_ID"'/g' "$APP_DIR/config/secrets.yml"
@@ -14,7 +12,5 @@ sed -i 's/{{ client_secret }}/'"$CLIENT_SECRET"'/g' "$APP_DIR/config/secrets.yml
 sed -i 's/{{ redirect_url }}/'"$REDIRECT_URL"'/g' "$APP_DIR/config/secrets.yml"
 sed -i 's/{{ secret_key_base }}/'"$SECRET_KEY_BASE"'/g' "$APP_DIR/config/secrets.yml"
 
-# exec puma -b "ssl://0.0.0.0:$PORT?key=self_signed.key&cert=self_signed.cert"
 echo "Start Service"
-# exec puma -b tcp://localhost:3000
 exec nginx
