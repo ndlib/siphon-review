91960713322d9195394958a88585a8398d0ac55c
Bill Sill <wsill@nd.edu>
2020-09-18 13:43:07 -0400

    Last test. Change back to sed and fix postman

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 set -e
 
-echo "Config files for database"
-perl -pe "s/\{\{ database_host \}\}/$DB_HOST/g" "$APP_DIR/config/database.yml"
-perl -pe "s/\{\{ database_username \}\}/$DB_NAME/g" "$APP_DIR/config/database.yml"
-perl -pe "s/\{\{ database_password \}\}/$DB_PASSWORD/g" "$APP_DIR/config/database.yml"
+echo "Modify config file for database"
+sed -i 's/{{ database_host }}/'"$DB_HOST"'/g' "$APP_DIR/config/database.yml"
+sed -i 's/{{ database_username }}/'"$DB_NAME"'/g' "$APP_DIR/config/database.yml"
+sed -i 's/{{ database_password }}/'"$DB_PASSWORD"'/g' "$APP_DIR/config/database.yml"
 
-echo "Config files for secrets"
-perl -pe "s/\{\{ auth_server_id \}\}/$AUTH_SERVER_ID/g" "$APP_DIR/config/secrets.yml"
-perl -pe "s/\{\{ client_id \}\}/$CLIENT_ID/g" "$APP_DIR/config/secrets.yml"
-perl -pe "s/\{\{ client_secret \}\}/$CLIENT_SECRET/g" "$APP_DIR/config/secrets.yml"
-perl -pe "s/\{\{ secret_key_base \}\}/$SECRET_KEY_BASE/g" "$APP_DIR/config/secrets.yml"
+echo "Modify config file for secrets"
+sed -i 's/{{ auth_server_id }}/'"$AUTH_SERVER_ID"'/g' "$APP_DIR/config/secrets.yml"
+sed -i 's/{{ client_id }}/'"$CLIENT_ID"'/g' "$APP_DIR/config/secrets.yml"
+sed -i 's/{{ client_secret }}/'"$CLIENT_SECRET"'/g' "$APP_DIR/config/secrets.yml"
+sed -i 's/{{ secret_key_base }}/'"$SECRET_KEY_BASE"'/g' "$APP_DIR/config/secrets.yml"
 
-echo "Config files for URL secrets"
-perl -pe "s/\{\{ base_auth_url \}\}/'"$BASE_AUTH_URL"'/g" "$APP_DIR/config/secrets.yml"
-perl -pe "s/\{\{ redirect_url \}\}/'"$REDIRECT_URL"'/g" "$APP_DIR/config/secrets.yml"
+echo "Modify config file for URL secrets"
+sed -i 's/{{ base_auth_url }}/'"$BASE_AUTH_URL"'/g' "$APP_DIR/config/secrets.yml"
+sed -i 's/{{ redirect_url }}/'"$REDIRECT_URL"'/g' "$APP_DIR/config/secrets.yml"
 
 echo "Start Service"
 # source /etc/nginx/envvars
