0ca32996535ba8a927d9bfa9b1adbd08e642b520
Bill Sill <wsill@nd.edu>
2020-09-18 13:58:04 -0400

    Test with hardcoded URLs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,8 +13,8 @@ sed -i 's/{{ client_secret }}/'"$CLIENT_SECRET"'/g' "$APP_DIR/config/secrets.yml
 sed -i 's/{{ secret_key_base }}/'"$SECRET_KEY_BASE"'/g' "$APP_DIR/config/secrets.yml"
 
 echo "Modify config file for URL secrets"
-sed -i 's/{{ base_auth_url }}/'"$BASE_AUTH_URL"'/g' "$APP_DIR/config/secrets.yml"
-sed -i 's/{{ redirect_url }}/'"$REDIRECT_URL"'/g' "$APP_DIR/config/secrets.yml"
+# sed -i 's/{{ base_auth_url }}/'"$BASE_AUTH_URL"'/g' "$APP_DIR/config/secrets.yml"
+# sed -i 's/{{ redirect_url }}/'"$REDIRECT_URL"'/g' "$APP_DIR/config/secrets.yml"
 
 echo "Start Service"
 # source /etc/nginx/envvars
