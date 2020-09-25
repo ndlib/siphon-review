997da88b9d911b2a44fda522997062cc6dbc9161
Bill Sill <wsill@nd.edu>
2020-09-25 13:08:39 -0400

    Fix missing parameter

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,10 @@ set -e
 
 echo "Modify config file for database"
 sed -i 's/{{ database_host }}/'"$DB_HOST"'/g' "$APP_DIR/config/database.yml"
-sed -i 's/{{ database_username }}/'"$DB_NAME"'/g' "$APP_DIR/config/database.yml"
+sed -i 's/{{ database_username }}/'"$DB_USER"'/g' "$APP_DIR/config/database.yml"
 sed -i 's/{{ database_password }}/'"$DB_PASSWORD"'/g' "$APP_DIR/config/database.yml"
+sed -i 's/{{ database_name }}/'"$DB_NAME"'/g' "$APP_DIR/config/database.yml"
+
 
 echo "Modify config file for secrets"
 sed -i 's/{{ auth_server_id }}/'"$AUTH_SERVER_ID"'/g' "$APP_DIR/config/secrets.yml"
@@ -18,3 +20,4 @@ sed -i 's/{{ host_name }}/'"$HOST_NAME"'/g' "$APP_DIR/config/secrets.yml"
 echo "Start Service"
 # exec nginx
 exec bundle exec passenger start
+# exec bundle exec rails server
